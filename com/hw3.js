555f7208662fa0592d352d084315f3f6cb0a5808
sysmaster <sysmaster@gmail.com>
2022-01-14 01:15:01 +0200

    hw3 ++

---
+++ com/hw3.js
@@ -0,0 +1,15 @@
+
+var val = 'd';
+if (val == 'a' ) {
+    console.log( 'a' );
+}else if (val == 'b' ) {
+    console.log( 'others' );
+}else if (val == 'c' ) {
+    console.log( 'others' );
+}else if (val == 'd' ) {
+    console.log( 'others' );
+}else if (val == 'e' ) {
+    console.log( 'others' );
+}
+else{ console.log( 'unknown' ); }
+
