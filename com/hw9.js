cab9e15175d55a728c8ea6e59e1a1b672e81d424
sysmaster <sysmaster@gmail.com>
2022-01-14 12:25:48 +0200

    ### Задача 9 Отсортировать массив по убыванию.

---
+++ com/hw9.js
@@ -0,0 +1,12 @@
+
+var arr = [1,2,3,4,5,6];
+for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+    for (var j = 0, endJ = endI - i; j < endJ; j++) {
+        if (arr[j] < arr[j + 1]) {
+            var swap = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = swap;
+        }
+    }
+}
+console.log(arr);
