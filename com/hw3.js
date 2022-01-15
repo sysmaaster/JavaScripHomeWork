377964bc22c4830d87b5691c9187dc685be37298
sysmaster <sysmaster@gmail.com>
2022-01-15 14:17:05 +0200

    hw3  rePush!

--- com/hw3.js
+++ com/hw3.js
@@ -1,13 +1,7 @@
 
 if (val == 'a' ) {
     console.log( 'a' );
-}else if (val == 'b' ) {
-    console.log( 'others' );
-}else if (val == 'c' ) {
-    console.log( 'others' );
-}else if (val == 'd' ) {
-    console.log( 'others' );
-}else if (val == 'e' ) {
+}else if (val == 'b' ||val == 'c' ||val == 'd' ||val == 'e' ) {
     console.log( 'others' );
 }
 else{ console.log( 'unknown' ); }
