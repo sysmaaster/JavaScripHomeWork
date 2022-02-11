5295da7d77dbe7311162d23a288a5e0b145b1c27
sysmaster <sysmaster@gmail.com>
2022-02-11 16:12:15 +0200

    ls5 Alpha

---
+++ com/ls5/h1.js
@@ -0,0 +1,23 @@
+/*
+#### Задача 1
+Напишите свою имплементацию функции `bind`
+
+Принимает такие параметры:
+- Первый параметр — функция которую мы хотим привязать
+- Второй параметр — ссылка на this
+- Третий параметр до ...n — параметры для функции которую мы хотим привязать
+
+Пример вызова:
+```javascript
+const obj = {
+getName: function(message) {
+return `${message}: ${this.name}`;
+}
+};
+
+const getName = obj.getName;
+
+const f = bind(getName, {name: 'Pitter'}, 'My name');
+console.log(f()); // My name: Pitter
+```
+*/
